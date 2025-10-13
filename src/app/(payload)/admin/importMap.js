47fef0075d646ff9f696aab9e6aa40cc992ebfd8
Kevin <kevinantoniowiyonolauw@mail.ugm.ac.id>
2025-10-13 14:36:14 +0700

    fix(importMap): fixed import map

--- src/app/(payload)/admin/importMap.js
+++ src/app/(payload)/admin/importMap.js
@@ -1,5 +1,7 @@
 import { default as default_b178031667217a2135783c33842b7f68 } from '../../../components/AdminLogoutButton'
+import { VercelBlobClientUploadHandler as VercelBlobClientUploadHandler_16c82c5e25f430251a3e3ba57219ff4e } from '@payloadcms/storage-vercel-blob/client'
 
 export const importMap = {
-  "./components/AdminLogoutButton#default": default_b178031667217a2135783c33842b7f68
+  "./components/AdminLogoutButton#default": default_b178031667217a2135783c33842b7f68,
+  "@payloadcms/storage-vercel-blob/client#VercelBlobClientUploadHandler": VercelBlobClientUploadHandler_16c82c5e25f430251a3e3ba57219ff4e
 }
