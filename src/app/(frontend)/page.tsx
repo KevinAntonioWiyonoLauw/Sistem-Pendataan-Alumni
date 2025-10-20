05f53b9ff0a3c112d66b1ef7f08e5779f7147582
Kevin <kevinantoniowiyonolauw@mail.ugm.ac.id>
2025-10-20 13:40:46 +0700

    fix(build): trying to build

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -4,9 +4,11 @@ import { InteractiveGridPattern } from '@/components/ui/interactive-grid-pattern
 export default function Home() {
   return (
     <div className="relative  w-screen  h-screen flex justify-center items-center">
-      <InteractiveGridPattern className='flex w-screen h-screen justify-center'/>
-      <div className='flex flex-col justify-center gap-4 items-center'>
-        <h1 className="z-2 text-4xl font-bold text-center ">Pendataan Alumni Computer Science UGM</h1>
+      <InteractiveGridPattern className="flex w-screen h-screen justify-center" />
+      <div className="flex flex-col justify-center gap-4 items-center">
+        <h1 className="z-2 text-4xl font-bold text-center ">
+          Pendataan Alumni Computer Science UGM
+        </h1>
         <p className="z-2 text-lg text-center text-gray-100">
           Terhubung dengan sesama alumni Computer Science Universitas Gadjah Mada
         </p>
@@ -17,14 +19,6 @@ export default function Home() {
               Lihat Daftar Alumni
             </button>
           </Link>
-
-          {/* <Link
-            href="/alumni/register"
-            className="bg-green-600 hover:bg-green-700 text-white px-6 py-3 rounded-lg inline-block"
-          >
-            Daftar Sebagai Alumni
-          </Link> */}
-
           <Link href="/admin">
             <button className="bg-gray-300 hover:bg-gray-400 text-black px-6 py-3 rounded-lg inline-block cursor-pointer">
               Admin Panel
